3541c0c24c9ec9e84ac688fa06cb019edb2656f8
Jason Champion <jchampion@zetacentauri.com>
2013-07-12 19:59:36 -0700

    Modded web.js

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var fs = require('fs');
-var buffer = new Buffer(fs.readFileSync('index,html'));
+var buf = new Buffer(fs.readFileSync('index.html'));
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString());
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
